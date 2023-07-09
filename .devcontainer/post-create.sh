d628a5a381d90c58f31f29f18be56c9b5ecbb96f
WalterMarch <walter.march@outlook.com>
2023-07-09 23:08:23 +0000

    vers 0

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /workspaces/wm-devcon-python/.devcontainer/configit.sh
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+exec bash -l
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# wm-devcon-python
-Visual Studio Code devcontainer for Python
+# wm-devcon-python - Visual Studio Code devcontainer for Python
+
+## Sample Dev Container file(s) for Python in VS Code Containerized Development
+
+*assumptions*: Docker and VS Code installed as well as the Dev Containers extension for Visual Studio Code.
+
+*usage*: clone the repo then open in Visual Studio Code.
+
+*example*:  clone the repo; open a new VS Code window; open the directory containing this repo; when prompted, choose Reopen in Container. 
+
+I chose to use a Ubuntu base and install `pyenv` to allow multiple version of Python.
+
+Based on https://code.visualstudio.com/docs/devcontainers/containers
+
+Jumping off point for `pyenv`: https://realpython.com/intro-to-pyenv/
