d876e9a7b61fd5ca5f2ee76a9181fc83a01995da
WalterMarch <walter.march@outlook.com>
2023-08-06 13:47:05 +0000

    updating devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /workspaces/wm-devcon-python/.devcontainer/configit.sh
+source $1/.devcontainer/configit.sh $1
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 curl https://pyenv.run | bash
 
